fc0a822effcf916cb8a0d93a8b968581ebab24bf
Kini <kinka96@abv.bg>
2015-02-23 08:41:10 +0200

    Update addBookCtrl.js

--- public/app/books/addBookCtrl.js
+++ public/app/books/addBookCtrl.js
@@ -1,5 +1,5 @@
 app.controller('AddBookCtrl', function($scope, $location, addbooks, notifier) {
-	$scope.addbook = function(addbooks) {
+	$scope.addbook = function(book) {
 		addbooks.addbook(book).then(function() {
 			notifier.success('Successful added!');
 			$location.path('/');
